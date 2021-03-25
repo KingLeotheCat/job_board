aea4e325ac9d194ca0cc6199c38898b5d8e0e51d
KingLeotheCat <lennylee91@gmail.com>
2021-03-24 23:06:04 -0700

    generate all resources

---
+++ db/migrate/20210325054829_create_companies.rb
@@ -0,0 +1,12 @@
+class CreateCompanies < ActiveRecord::Migration[6.1]
+  def change
+    create_table :companies do |t|
+      t.string :title
+      t.text :content
+      t.belongs_to :user, null: false, foreign_key: true
+      t.belongs_to :category, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
